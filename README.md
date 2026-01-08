45317088262e338e513c18525aa8b1990dbb6be9
guorunli <guorunli0417@163.com>
2026-01-08 10:23:09 +0800

    submit figures folder

---
+++ README.md
@@ -0,0 +1,137 @@
+# Diffusion-Tuning
+This is the code base for **`"Step-by-step Fine-Tuning Strategy for Deep Neural Networks"`**, and the article is submitted to **`Knowledge-Based Systems`**.
+
+## Abstract
+As data size and computational power increase, the pre-training + fine-tuning paradigm emerges in deep learning, where fine-tuning determines the model's performance on a new target task. Currently, mainstream methods, such as full fine-tuning (FFT) or parameter-efficient fine-tuning (PEFT), uniformly optimize all training parameters within a specified epoch, collectively referred to as one-step tuning (OST). However, OST encounters the challenge of model performance failing to improve with increased training time, tending to saturate or even decline at some point. Consequently, we introduce the concept of step-by-step tuning (SST), which refers to the training method of fine-tuning a model by progressively optimizing the parameters of different parts of the model in a specific order or strategy, allowing the model to be better adapted to new tasks. Inspired by the physical diffusion process, we propose a novel SST method, diffusion tuning, which divides the neural network parameter space into multiple subspaces and progressively optimizes each using a staged training strategy. Experiments conducted on three visual models and two language models demonstrate that diffusion tuning effectively reduces the difficulty of finding the global optimum during training and enhances the baseline model's performance across various tasks with good generalization. Finally, we develop AgriMaTech-LLaMA, a large-scale language model for agricultural mechanization education based on diffusion tuning, and verified its practical value.
+
+![diffusion](figures/fig1.png)
+![hypothetical](figures/fig2.png)
+
+## Results
+**To learn more detail please read our paper**.
+![cvresult](figures/fig5.png)
+![nlpresult1](figures/fig6.png)
+![nlpresult2](figures/fig7.png)
+![cvtrainresult](figures/fig8.png)
+![cvresultvis](figures/fig9.png)
+
+## Train and Test
+We have provided detailed instructions for model training and testing and experimental details. 
+### Install
+- Clone this repo:
+
+```bash
+conda create -n dt python=3.10 -y
+conda activate dt
+git clone https://github.com/CAU-COE-VEICLab/Diffusion-Tuning.git
+cd Diffusion-Tuning
+```
+- Install `CUDA>=10.2` with `cudnn>=7` following
+  the [official installation instructions](https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html)
+- Install `PyTorch>=1.8.0` and `torchvision>=0.9.0` with `CUDA>=10.2`:
+
+```bash
+pip install torch==1.10.0+cu111 torchvision==0.11.0+cu111 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install timm==0.4.12
+pip install opencv-python==4.4.0.46 termcolor==1.1.0 yacs==0.1.8 pyyaml scipy
+```
+
+
+### Data preparation
+
+We use standard ImageNet dataset, you can download it from http://image-net.org/. We provide the following two ways to
+load data:
+
+- For standard folder dataset, move validation images to labeled sub-folders. The file structure should look like:
+  ```bash
+  $ tree data
+  imagenet
+  ├── train
+  │   ├── class1
+  │   │   ├── img1.jpeg
+  │   │   ├── img2.jpeg
+  │   │   └── ...
+  │   ├── class2
+  │   │   ├── img3.jpeg
+  │   │   └── ...
+  │   └── ...
+  └── val
+      ├── class1
+      │   ├── img4.jpeg
+      │   ├── img5.jpeg
+      │   └── ...
+      ├── class2
+      │   ├── img6.jpeg
+      │   └── ...
+      └── ...
+ 
+  ```
+
+### Evaluation
+
+To evaluate a pre-trained `Swin Transformer` on ImageNet val, run:
+
+```bash
+python -m torch.distributed.launch --nproc_per_node <num-of-gpus-to-use>  main_diffusion_tuning.py --eval \
+--cfg <config-file> --pretrained <checkpoint> --data-path <imagenet-path> 
+```
+
+**Notes**:
+
+- Please note that when testing the results of Diffusion Tuning (DT1~DT4), select the configuration file (--cfg) of the original model (the yaml file that does not contain 'dt').
+
+For example, to evaluate the `Swin-B-DT1` with a single GPU:
+
+```bash
+python -m torch.distributed.launch --nproc_per_node 1 main_diffusion_tuning.py --eval \
+--cfg configs/diffusion_finetune/swin/swin/swin_base_patch4_window7_224_22kto1k_finetune.yaml --pretrained dt1_swin_base_patch4_window7_224_22k.pth --data-path <imagenet-path>
+```
+
+### Full Fine-tuning (FFT) from a ImageNet-22K(21K) pre-trained model
+
+For example, using DT-1 as the training method to fine-tune a Swin-B model pre-trained on ImageNet-22K (21K):
+
+```bashs
+python -m torch.distributed.launch --nproc_per_node 8 main_diffusion_tuning.py \
+--cfg configs/diffusion_finetune/swin/swin/dt1_swin_base_patch4_window7_224_22kto1k.yaml --pretrained swin_base_patch4_window7_224_22k.pth \
+--data-path <imagenet-path> --batch-size 64 --accumulation-steps 2 --use-checkpoint --output <ouput-path> --tag swin_base_dt1
+```
+
+For example, using OST(one step tuning) as the training method to fine-tune a Swin-B model pre-trained on ImageNet-22K (21K):
+
+```bashs
+python -m torch.distributed.launch --nproc_per_node 8 main_diffusion_tuning.py \
+--cfg configs/diffusion_finetune/swin/swin/swin_base_patch4_window7_224_22kto1k_finetune.yaml --pretrained swin_base_patch4_window7_224_22k.pth \
+--data-path <imagenet-path> --batch-size 64 --accumulation-steps 2 --use-checkpoint --output <ouput-path> --tag swin_base_ost
+```
+
+### Parameter-Efficient Fine-tuning (PEFT) from a ImageNet-22K(21K) pre-trained model
+
+For example, using DT-1 as the training method to fine-tune a Swin-B-Adapter model pre-trained on ImageNet-22K (21K):
+
+```bashs
+python -m torch.distributed.launch --nproc_per_node 8 main_diffusion_tuning.py \
+--cfg configs/diffusion_finetune/swin/adapter-swin/dt1_adapter_swin_base_patch4_window7_224_22kto1k.yaml --pretrained swin_base_patch4_window7_224_22k.pth \
+--data-path <imagenet-path> --batch-size 64 --accumulation-steps 2 --use-checkpoint --output <ouput-path> --tag swin_base_adapter_dt1
+```
+
+For example, using OST(one step tuning) as the training method to fine-tune a Swin-B model pre-trained on ImageNet-22K (21K):
+
+```bashs
+python -m torch.distributed.launch --nproc_per_node 8 main_diffusion_tuning.py \
+--cfg configs/diffusion_finetune/swin/adapter-swin/adapter_swin_base_patch4_window7_224_22kto1k.yaml --pretrained swin_base_patch4_window7_224_22k.pth \
+--data-path <imagenet-path> --batch-size 64 --accumulation-steps 2 --use-checkpoint --output <ouput-path> --tag swin_base_adapter_ost
+```
+## Practical Application
+
+### AgriMaTech-LLaMA
+
+To validate the effectiveness of diffusion tuning in specific tasks, we constructed a vertical domain supervised fine-tuning dataset for agricultural mechanization specialization - AgriMachine28K.  We then used LLaMA3.1-8B-Instruct as a base model and DT-3 as a training method on the AgriMachine28K and fine-tuning the base model to obtain AgriMaTech-LLaMA, a large language model in the field of assisted learning and teaching.  
+
+you can click this [link](https://drive.google.com/drive/folders/1UYfqghaAWC0uqddyE6odlaGjKrjlsvQR?usp=drive_link) to download AgriMaTech-LLaMA.
+
+**Notes**:
+
+- The weights provided in the link are exclusively for LORA (Low-Rank Adaptation). You can load these weights using peft.PeftModel.from_pretrained provided by Hugging Face.
+
+## Citation
---
+++ models/build.py
@@ -0,0 +1,299 @@
+# --------------------------------------------------------
+# Diffusion Tuning
+# Copyright (c) 2026 CAU
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Guorun Li
+# --------------------------------------------------------
+
+from .swin_transformer import SwinTransformer
+from .smt import SMT
+from .convnext import ConvNeXt
+
+from .adapter_swin_transformer import Adapter_SwinTransformer
+from .adapter_convnext import Adapter_ConvNeXt
+from .adapter_smt import Adapter_SMT
+
+from .swin_transformer_diffusion_finetune import SwinTransformer_Diffusion_Finetune
+from .convnext_diffusion_finetune import ConvNeXt_Diffusion_Finetune
+from .smt_diffusion_finetune import SMT_Diffusion_Finetune
+
+from .adapter_swin_transformer_diffusion_finetune import Adapter_SwinTransformer_Diffusion_Finetune
+from .adapter_convnext_diffusion_finetune import Adapter_ConvNeXt_Diffusion_Finetune
+from .adapter_smt_diffusion_finetune import Adapter_SMT_Diffusion_Finetune
+
+
+
+def build_model(config, is_pretrain=False):
+    model_type = config.MODEL.TYPE
+
+    # accelerate layernorm
+    if config.FUSED_LAYERNORM:
+        try:
+            import apex as amp
+            layernorm = amp.normalization.FusedLayerNorm
+        except:
+            layernorm = None
+            print("To use FusedLayerNorm, please install apex.")
+    else:
+        import torch.nn as nn
+        layernorm = nn.LayerNorm
+
+
+    if model_type == 'swin':
+        model = SwinTransformer(img_size=config.DATA.IMG_SIZE,
+                                patch_size=config.MODEL.SWIN.PATCH_SIZE,
+                                in_chans=config.MODEL.SWIN.IN_CHANS,
+                                num_classes=config.MODEL.NUM_CLASSES,
+                                embed_dim=config.MODEL.SWIN.EMBED_DIM,
+                                depths=config.MODEL.SWIN.DEPTHS,
+                                num_heads=config.MODEL.SWIN.NUM_HEADS,
+                                window_size=config.MODEL.SWIN.WINDOW_SIZE,
+                                mlp_ratio=config.MODEL.SWIN.MLP_RATIO,
+                                qkv_bias=config.MODEL.SWIN.QKV_BIAS,
+                                qk_scale=config.MODEL.SWIN.QK_SCALE,
+                                drop_rate=config.MODEL.DROP_RATE,
+                                drop_path_rate=config.MODEL.DROP_PATH_RATE,
+                                ape=config.MODEL.SWIN.APE,
+                                norm_layer=layernorm,
+                                patch_norm=config.MODEL.SWIN.PATCH_NORM,
+                                use_checkpoint=config.TRAIN.USE_CHECKPOINT,
+                                fused_window_process=config.FUSED_WINDOW_PROCESS)
+
+    elif model_type == 'adapter_swin':
+        model = Adapter_SwinTransformer(is_efficient_finetune=config.TRAIN.EFFICIENT_FINETUNE,
+                                     pretrain_image_size=config.MODEL.VCNU_SWIN.PRETRAIN_IMAGE_SIZE,
+                                     model_style=config.MODEL.VCNU_SWIN.MODEL_STYLE,
+                                     training_mode=config.MODEL.VCNU_SWIN.TRAINING_MODE,
+                                     use_layerscales=config.MODEL.VCNU_SWIN.USE_LAYERSCALE,
+                                     layer_scale_init_value=config.MODEL.VCNU_SWIN.LAYER_SCALE_INIT_VALUE,
+
+                                     img_size=config.DATA.IMG_SIZE,
+                                     patch_size=config.MODEL.VCNU_SWIN.PATCH_SIZE,
+                                     # out_dim=config.MODEL.VCNU_SWIN.OUT_DIM,
+                                     in_chans=config.MODEL.VCNU_SWIN.IN_CHANS,
+                                     num_classes=config.MODEL.NUM_CLASSES,
+                                     embed_dim=config.MODEL.VCNU_SWIN.EMBED_DIM,
+                                     depths=config.MODEL.VCNU_SWIN.DEPTHS,
+                                     num_heads=config.MODEL.VCNU_SWIN.NUM_HEADS,
+                                     window_size=config.MODEL.VCNU_SWIN.WINDOW_SIZE,
+                                     mlp_ratio=config.MODEL.VCNU_SWIN.MLP_RATIO,
+                                     qkv_bias=config.MODEL.VCNU_SWIN.QKV_BIAS,
+                                     qk_scale=config.MODEL.VCNU_SWIN.QK_SCALE,
+                                     drop_rate=config.MODEL.DROP_RATE,
+                                     drop_path_rate=config.MODEL.DROP_PATH_RATE,
+                                     ape=config.MODEL.VCNU_SWIN.APE,
+                                     norm_layer=layernorm,
+                                     patch_norm=config.MODEL.VCNU_SWIN.PATCH_NORM,
+                                     use_checkpoint=config.TRAIN.USE_CHECKPOINT,
+                                     fused_window_process=config.FUSED_WINDOW_PROCESS)
+    elif model_type == 'smt':
+        model = SMT(
+            img_size=config.DATA.IMG_SIZE,
+            in_chans=config.MODEL.VCNU_SMT.IN_CHANS,
+            num_classes=config.MODEL.NUM_CLASSES,
+            embed_dims=config.MODEL.VCNU_SMT.EMBED_DIMS,
+            ca_num_heads=config.MODEL.VCNU_SMT.CA_NUM_HEADS,
+            sa_num_heads=config.MODEL.VCNU_SMT.SA_NUM_HEADS,
+            mlp_ratios=config.MODEL.VCNU_SMT.MLP_RATIOS,
+            qkv_bias=config.MODEL.VCNU_SMT.QKV_BIAS,
+            qk_scale=config.MODEL.VCNU_SMT.QK_SCALE,
+            use_layerscale=config.MODEL.VCNU_SMT.USE_LAYERSCALE,  #
+            depths=config.MODEL.VCNU_SMT.DEPTHS,
+            ca_attentions=config.MODEL.VCNU_SMT.CA_ATTENTIONS,
+            head_conv=config.MODEL.VCNU_SMT.HEAD_CONV,
+            expand_ratio=config.MODEL.VCNU_SMT.EXPAND_RATIO,
+            drop_rate=config.MODEL.DROP_RATE,
+            drop_path_rate=config.MODEL.DROP_PATH_RATE,
+            use_checkpoint=config.TRAIN.USE_CHECKPOINT)
+
+    elif model_type == 'adapter_smt':
+        model = Adapter_SMT(
+            is_efficient_finetune=config.TRAIN.EFFICIENT_FINETUNE,
+            patch_size=config.MODEL.VCNU_SMT.PATCH_SIZE,
+            model_style=config.MODEL.VCNU_SMT.MODEL_STYLE,
+
+            use_layerscale=config.MODEL.VCNU_SMT.USE_LAYERSCALE,
+            layerscale_value=config.MODEL.VCNU_SMT.LAYERSCALE_VALUE,  #
+
+            img_size=config.DATA.IMG_SIZE,
+            in_chans=config.MODEL.VCNU_SMT.IN_CHANS,
+            num_classes=config.MODEL.NUM_CLASSES,
+            embed_dims=config.MODEL.VCNU_SMT.EMBED_DIMS,
+            ca_num_heads=config.MODEL.VCNU_SMT.CA_NUM_HEADS,
+            sa_num_heads=config.MODEL.VCNU_SMT.SA_NUM_HEADS,
+            mlp_ratios=config.MODEL.VCNU_SMT.MLP_RATIOS,
+            qkv_bias=config.MODEL.VCNU_SMT.QKV_BIAS,
+            qk_scale=config.MODEL.VCNU_SMT.QK_SCALE,
+            depths=config.MODEL.VCNU_SMT.DEPTHS,
+            ca_attentions=config.MODEL.VCNU_SMT.CA_ATTENTIONS,
+            head_conv=config.MODEL.VCNU_SMT.HEAD_CONV,
+            expand_ratio=config.MODEL.VCNU_SMT.EXPAND_RATIO,
+            drop_rate=config.MODEL.DROP_RATE,
+            drop_path_rate=config.MODEL.DROP_PATH_RATE,
+            use_checkpoint=config.TRAIN.USE_CHECKPOINT)
+
+    elif model_type == 'convnext':
+        model = ConvNeXt(
+            in_chans=config.MODEL.VCNU_CONVNEXT.IN_CHANS,
+            num_classes=config.MODEL.NUM_CLASSES,
+            depths=config.MODEL.VCNU_CONVNEXT.DEPTHS,
+            dims=config.MODEL.VCNU_CONVNEXT.DIMS,
+            drop_path_rate=config.MODEL.DROP_PATH_RATE,
+            layer_scale_init_value=config.MODEL.VCNU_CONVNEXT.LAYER_SCALE_INIT_VALUE,
+            )
+
+    elif model_type == 'adapter_convnext':
+        model = Adapter_ConvNeXt(
+            is_efficient_finetune=config.TRAIN.EFFICIENT_FINETUNE,
+            patch_size=config.MODEL.VCNU_CONVNEXT.PATCH_SIZE,
+            img_size=config.DATA.IMG_SIZE,
+            model_style=config.MODEL.VCNU_CONVNEXT.MODEL_STYLE,
+
+            in_chans=config.MODEL.VCNU_CONVNEXT.IN_CHANS,
+            num_classes=config.MODEL.NUM_CLASSES,
+            depths=config.MODEL.VCNU_CONVNEXT.DEPTHS,
+            dims=config.MODEL.VCNU_CONVNEXT.DIMS,
+            drop_path_rate=config.MODEL.DROP_PATH_RATE,
+            layer_scale_init_value=config.MODEL.VCNU_CONVNEXT.LAYER_SCALE_INIT_VALUE,
+            use_checkpoint=config.TRAIN.USE_CHECKPOINT)
+# --------------------------------------diffusion tuning -------------------------------------------
+    elif model_type == 'swin_diffusion_finetune':
+        model = SwinTransformer_Diffusion_Finetune(
+            finetune_mode=config.TRAIN.FINETUNE_MODE,
+            is_efficient_finetune=config.TRAIN.EFFICIENT_FINETUNE,
+
+            img_size=config.DATA.IMG_SIZE,
+            patch_size=config.MODEL.SWIN.PATCH_SIZE,
+            in_chans=config.MODEL.SWIN.IN_CHANS,
+            num_classes=config.MODEL.NUM_CLASSES,
+            embed_dim=config.MODEL.SWIN.EMBED_DIM,
+            depths=config.MODEL.SWIN.DEPTHS,
+            num_heads=config.MODEL.SWIN.NUM_HEADS,
+            window_size=config.MODEL.SWIN.WINDOW_SIZE,
+            mlp_ratio=config.MODEL.SWIN.MLP_RATIO,
+            qkv_bias=config.MODEL.SWIN.QKV_BIAS,
+            qk_scale=config.MODEL.SWIN.QK_SCALE,
+            drop_rate=config.MODEL.DROP_RATE,
+            drop_path_rate=config.MODEL.DROP_PATH_RATE,
+            ape=config.MODEL.SWIN.APE,
+            norm_layer=layernorm,
+            patch_norm=config.MODEL.SWIN.PATCH_NORM,
+            use_checkpoint=config.TRAIN.USE_CHECKPOINT,
+            fused_window_process=config.FUSED_WINDOW_PROCESS)
+
+    elif model_type == 'adapter_swin_diffusion_finetune':
+        model = Adapter_SwinTransformer_Diffusion_Finetune(
+             add_extra_adapter=config.MODEL.VCNU_SWIN.ADD_EXTRA_ADAPTER,
+             finetune_mode=config.TRAIN.FINETUNE_MODE,
+             is_efficient_finetune=config.TRAIN.EFFICIENT_FINETUNE,
+             pretrain_image_size=config.MODEL.VCNU_SWIN.PRETRAIN_IMAGE_SIZE,
+             model_style=config.MODEL.VCNU_SWIN.MODEL_STYLE,
+             training_mode=config.MODEL.VCNU_SWIN.TRAINING_MODE,
+             use_layerscales=config.MODEL.VCNU_SWIN.USE_LAYERSCALE,
+             layer_scale_init_value=config.MODEL.VCNU_SWIN.LAYER_SCALE_INIT_VALUE,
+
+             img_size=config.DATA.IMG_SIZE,
+             patch_size=config.MODEL.VCNU_SWIN.PATCH_SIZE,
+             # out_dim=config.MODEL.VCNU_SWIN.OUT_DIM,
+             in_chans=config.MODEL.VCNU_SWIN.IN_CHANS,
+             num_classes=config.MODEL.NUM_CLASSES,
+             embed_dim=config.MODEL.VCNU_SWIN.EMBED_DIM,
+             depths=config.MODEL.VCNU_SWIN.DEPTHS,
+             num_heads=config.MODEL.VCNU_SWIN.NUM_HEADS,
+             window_size=config.MODEL.VCNU_SWIN.WINDOW_SIZE,
+             mlp_ratio=config.MODEL.VCNU_SWIN.MLP_RATIO,
+             qkv_bias=config.MODEL.VCNU_SWIN.QKV_BIAS,
+             qk_scale=config.MODEL.VCNU_SWIN.QK_SCALE,
+             drop_rate=config.MODEL.DROP_RATE,
+             drop_path_rate=config.MODEL.DROP_PATH_RATE,
+             ape=config.MODEL.VCNU_SWIN.APE,
+             norm_layer=layernorm,
+             patch_norm=config.MODEL.VCNU_SWIN.PATCH_NORM,
+             use_checkpoint=config.TRAIN.USE_CHECKPOINT,
+             fused_window_process=config.FUSED_WINDOW_PROCESS)
+
+    elif model_type == 'convnext_diffusion_finetune':
+        model = ConvNeXt_Diffusion_Finetune(
+            finetune_mode=config.TRAIN.FINETUNE_MODE,
+            is_efficient_finetune=config.TRAIN.EFFICIENT_FINETUNE,
+
+            in_chans=config.MODEL.VCNU_CONVNEXT.IN_CHANS,
+            num_classes=config.MODEL.NUM_CLASSES,
+            depths=config.MODEL.VCNU_CONVNEXT.DEPTHS,
+            dims=config.MODEL.VCNU_CONVNEXT.DIMS,
+            drop_path_rate=config.MODEL.DROP_PATH_RATE,
+            layer_scale_init_value=config.MODEL.VCNU_CONVNEXT.LAYER_SCALE_INIT_VALUE,
+            )
+
+    elif model_type == 'adapter_convnext_diffusion_finetune':
+        model = Adapter_ConvNeXt_Diffusion_Finetune(
+            add_extra_adapter=config.MODEL.VCNU_CONVNEXT.ADD_EXTRA_ADAPTER,
+            finetune_mode=config.TRAIN.FINETUNE_MODE,
+            is_efficient_finetune=config.TRAIN.EFFICIENT_FINETUNE,
+            patch_size=config.MODEL.VCNU_CONVNEXT.PATCH_SIZE,
+            img_size=config.DATA.IMG_SIZE,
+            model_style=config.MODEL.VCNU_CONVNEXT.MODEL_STYLE,
+
+            in_chans=config.MODEL.VCNU_CONVNEXT.IN_CHANS,
+            num_classes=config.MODEL.NUM_CLASSES,
+            depths=config.MODEL.VCNU_CONVNEXT.DEPTHS,
+            dims=config.MODEL.VCNU_CONVNEXT.DIMS,
+            drop_path_rate=config.MODEL.DROP_PATH_RATE,
+            layer_scale_init_value=config.MODEL.VCNU_CONVNEXT.LAYER_SCALE_INIT_VALUE,
+            use_checkpoint=config.TRAIN.USE_CHECKPOINT)
+
+    elif model_type == 'smt_diffusion_finetune':
+        model = SMT_Diffusion_Finetune(
+            finetune_mode=config.TRAIN.FINETUNE_MODE,
+            is_efficient_finetune=config.TRAIN.EFFICIENT_FINETUNE,
+
+            img_size=config.DATA.IMG_SIZE,
+            in_chans=config.MODEL.VCNU_SMT.IN_CHANS,
+            num_classes=config.MODEL.NUM_CLASSES,
+            embed_dims=config.MODEL.VCNU_SMT.EMBED_DIMS,
+            ca_num_heads=config.MODEL.VCNU_SMT.CA_NUM_HEADS,
+            sa_num_heads=config.MODEL.VCNU_SMT.SA_NUM_HEADS,
+            mlp_ratios=config.MODEL.VCNU_SMT.MLP_RATIOS,
+            qkv_bias=config.MODEL.VCNU_SMT.QKV_BIAS,
+            qk_scale=config.MODEL.VCNU_SMT.QK_SCALE,
+            use_layerscale=config.MODEL.VCNU_SMT.USE_LAYERSCALE,  #
+            depths=config.MODEL.VCNU_SMT.DEPTHS,
+            ca_attentions=config.MODEL.VCNU_SMT.CA_ATTENTIONS,
+            head_conv=config.MODEL.VCNU_SMT.HEAD_CONV,
+            expand_ratio=config.MODEL.VCNU_SMT.EXPAND_RATIO,
+            drop_rate=config.MODEL.DROP_RATE,
+            drop_path_rate=config.MODEL.DROP_PATH_RATE,
+            use_checkpoint=config.TRAIN.USE_CHECKPOINT)
+
+    elif model_type == 'adapter_smt_diffusion_finetune':
+        model = Adapter_SMT_Diffusion_Finetune(
+            finetune_mode=config.TRAIN.FINETUNE_MODE,
+            is_efficient_finetune=config.TRAIN.EFFICIENT_FINETUNE,
+
+            patch_size=config.MODEL.VCNU_SMT.PATCH_SIZE,
+            model_style=config.MODEL.VCNU_SMT.MODEL_STYLE,
+
+            use_layerscale=config.MODEL.VCNU_SMT.USE_LAYERSCALE,
+            layerscale_value=config.MODEL.VCNU_SMT.LAYERSCALE_VALUE,  #
+
+            img_size=config.DATA.IMG_SIZE,
+            in_chans=config.MODEL.VCNU_SMT.IN_CHANS,
+            num_classes=config.MODEL.NUM_CLASSES,
+            embed_dims=config.MODEL.VCNU_SMT.EMBED_DIMS,
+            ca_num_heads=config.MODEL.VCNU_SMT.CA_NUM_HEADS,
+            sa_num_heads=config.MODEL.VCNU_SMT.SA_NUM_HEADS,
+            mlp_ratios=config.MODEL.VCNU_SMT.MLP_RATIOS,
+            qkv_bias=config.MODEL.VCNU_SMT.QKV_BIAS,
+            qk_scale=config.MODEL.VCNU_SMT.QK_SCALE,
+            depths=config.MODEL.VCNU_SMT.DEPTHS,
+            ca_attentions=config.MODEL.VCNU_SMT.CA_ATTENTIONS,
+            head_conv=config.MODEL.VCNU_SMT.HEAD_CONV,
+            expand_ratio=config.MODEL.VCNU_SMT.EXPAND_RATIO,
+            drop_rate=config.MODEL.DROP_RATE,
+            drop_path_rate=config.MODEL.DROP_PATH_RATE,
+            use_checkpoint=config.TRAIN.USE_CHECKPOINT)
+
+    else:
+        raise NotImplementedError(f"Unkown model: {model_type}")
+
+    return model
